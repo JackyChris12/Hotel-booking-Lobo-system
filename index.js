3e985de03e65c884ad980250ae5fedda8959656f
JackyChris12 <biwottjackline72@gmail.com>
2025-05-02 10:14:59 +0300

    new

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 const dbConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -17,6 +26,18 @@ app.use(express.static(path.join(__dirname, "public"))); //path --nested routes 
 app.use(express.urlencoded({ extended: true })); //for persing application
 
 //routes
+
+//login routes
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", express({ extended: true }), (req, res) => {
+  const { email, password } = req.body;
+  dbConnection.query("SELECT * FROM users WHERE email = ?", [email], async (error, results){
+    if(err || results.length ===0 ) return res.status(404).send('Inavild credetial')
+  })
+});
 
 app.get("/", (req, res) => {
   dbConnection.query("SELECT * FROM rooms", (roomsSelectError, rooms) => {
